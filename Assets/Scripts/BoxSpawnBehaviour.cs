12f74ba19b23b49bab4e11f1ec1f853720032018
Nicholas Arnaud <cobrahead09@gmail.com>
2017-04-26 17:58:23 -0500

    Added TBA
    Turn Based Adventure

---
+++ Assets/Scripts/BoxSpawnBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Spawn Boxes Behaviour
+public class GameBehaviour : MonoBehaviour
+{
+    public List<GameObject> boxes;
+    public GameObject boxPrefab;
+	
+    void Start()
+    {
+        boxes = new List<GameObject>();
+    }
+
+    void Update()
+    {
+        GameObject box;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            box = Instantiate(boxPrefab) as GameObject;
+            boxes.Add(box);
+        }
+    }
+
+}
