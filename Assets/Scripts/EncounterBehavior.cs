5c914f36cb1da24bbed7932f3f5bffc7d6c060b2
Nicholas Arnaud <cobrahead09@gmail.com>
2017-04-28 17:58:26 -0500

    Onentertrigger practice

---
+++ Assets/Scripts/EncounterBehavior.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EncounterBehavior : MonoBehaviour
+{
+    public Color originalColor;
+    Material parentMaterial;
+    public GameObject canvasGameObject;
+
+    private void Start()
+    {
+        parentMaterial = GetComponentInParent<MeshRenderer>().materials[0];
+        originalColor = parentMaterial.color;
+        canvasGameObject = FindObjectOfType<Canvas>().gameObject;
+        if (canvasGameObject == null)
+            return;
+        canvasGameObject.SetActive(false);
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            parentMaterial.color = Random.ColorHSV();
+
+            canvasGameObject.SetActive(true);
+
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            parentMaterial.color = originalColor;
+
+          //  canvasGameObject.SetActive(true);
+
+        }
+    }
+}
---
+++ Assets/Scripts/GridBehavior.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridBehavior : MonoBehaviour
+{
+
+    public List<GameObject> boxes;
+    public GameObject boxPrefab;
+    public float offset;
+    public int gridSize;
+
+    // Use this for initialization
+    void Start()
+    {
+        boxes = new List<GameObject>();
+        Create();
+    }
+
+
+    [ContextMenu("Create")]
+    void Create()
+    {
+        for (int x = 0 ; x < gridSize; x++)
+        {
+            for (int z = 0; z< gridSize; z++)
+            {
+                var pos = new Vector3(x * offset, 0, z * offset);
+                var quat = new Quaternion(0 ,0 ,0, 0);
+                GameObject box;
+                box = Instantiate(boxPrefab, pos, quat) as GameObject;
+                boxes.Add(box);
+            }
+        }
+    }
+
+    [ContextMenu("Destroy")]
+    void Destroy()
+    {
+        foreach(GameObject box in boxes)
+        {
+            DestroyImmediate(box);
+        }
+        boxes.Clear();
+    }
+
+}
